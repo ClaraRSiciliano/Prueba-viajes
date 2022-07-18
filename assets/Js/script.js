d05360ce922ae74acaf9129d4b5ca32f90d9aee7
ClaraRSiciliano <clararodriguezsiciliano@gmail.com>
2022-07-18 10:56:14 -0400

    Se agrego otra funcion en js, se ordena el html y se corrije la visualizacion del html en el css

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,3 +1,14 @@
+// Al hacer doble click en los titulos de de las secciones "Quienes somos" y "Destacados" , cambian de color
+$('#quienes').on('dblclick',function(){
+    $('#quienes').css({'color':'aquamarine'});
+});
+
+$('#destacados').on('dblclick',function(){
+    $('#destacados').css({'color':'aquamarine'});
+});
+
+
+
 // Ocultar texto de las cards al hacer click en cualquiera de las imagenes - Sección destacados
 $('.card-img-top').on('click', function(){
     $('.card-text').toggle('fast', function(){
