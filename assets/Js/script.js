e843dc91cbe084c8b1c12e46afafd29e9ad34f0a
ClaraRSiciliano <clararodriguezsiciliano@gmail.com>
2022-07-18 10:12:00 -0400

    Se agrega otra función de js y se agrega titulo a seccion de destacados

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,4 +1,13 @@
+// Ocultar texto de las cards al hacer click en cualquiera de las imagenes - Sección destacados
+$('.card-img-top').on('click', function(){
+    $('.card-text').toggle('fast', function(){
+
+    });
+});
+
+// alerta al presionar boton de enviar - Sección contacto
 var botonmensaje = $('#enviarmensaje');
 botonmensaje.on('click', function(){
     alert("¡Tu mensaje fue enviado exitosamente!");
 });
+
